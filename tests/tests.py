5c232eeb0b83713321835bee044b133c3c7a1092
Blake Dewey <blake.dewey@nih.gov>
2016-04-01 11:38:00 -0400

    Changed to OOP structure. Added new options. Changed tests to use new structure

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,7 @@
 import unittest
 import dicom
-import sys
 import dicom_anon
+
 
 class TestDICOMAnon(unittest.TestCase):
 
@@ -11,11 +11,13 @@ class TestDICOMAnon(unittest.TestCase):
     def test_basic(self):
         ds = dicom.read_file("tests/samples/test_wrist_cr1.dcm")
         self.assertEqual(ds.PatientName, "Identified Patient")
-        dicom_anon.driver("tests/samples", "tests/clean", quarantine_dir="quarantine", audit_file="identity.db",
-            allowed_modalities=["us","cr","ct", "mr", "pr"], org_root="1.2.826.0.1.3680043.8.1008",
-            white_list_file="white_list.json", log_file=None, rename=False,profile="basic", overlay=False)
+        da = dicom_anon.DicomAnon(quarantine="quarantine", audit_file="identity.db",
+                                  modalities=["us", "cr", "ct", "mr", "pr"], org_root="1.2.826.0.1.3680043.8.1008",
+                                  white_list="white_list.json", log_file=None, rename=False, profile="basic",
+                                  overlay=False)
+        da.run("tests/samples", "tests/clean")
         ds = dicom.read_file("tests/clean/test_wrist_cr1.dcm")
-        self.assertEqual(ds.PatientName,  "Patient's Name 1")
+        self.assertEqual(ds.PatientName, "Patient's Name 1")
         self.assertEqual(ds.StudyID, "CLEANED")
         # Not using clean descriptions, so study and series description should be gone
         self.assertFalse(dicom_anon.SERIES_DESCR in ds)
@@ -24,11 +26,14 @@ class TestDICOMAnon(unittest.TestCase):
     def test_clean_option(self):
         ds = dicom.read_file("tests/samples/test_wrist_cr1.dcm")
         self.assertEqual(ds.PatientName, "Identified Patient")
-        dicom_anon.driver("tests/samples", "tests/clean", quarantine_dir="quarantine", audit_file="identity.db",
-            allowed_modalities=["us","ct", "cr","mr","pr"], org_root="1.2.826.0.1.3680043.8.1008",
-            white_list_file="white_list.json", log_file=None, rename=False,profile="clean", overlay=False)
+        da = dicom_anon.DicomAnon(quarantine="quarantine", audit_file="identity.db",
+                                  modalities=["us", "cr", "ct", "mr", "pr"], org_root="1.2.826.0.1.3680043.8.1008",
+                                  white_list="white_list.json", log_file=None, rename=False, profile="clean",
+                                  overlay=False)
+
+        da.run("tests/samples", "tests/clean")
         ds = dicom.read_file("tests/clean/test_wrist_cr1.dcm")
-        self.assertEqual(ds.PatientName,"Patient's Name 1")
+        self.assertEqual(ds.PatientName, "Patient's Name 1")
         # Study Description was in whitelist so it should have stayed 
         self.assertTrue(dicom_anon.STUDY_DESCR in ds)
         self.assertEqual(ds.StudyDescription, "WRIST MIN 3V UNILAT")
